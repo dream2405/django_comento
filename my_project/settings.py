9dce6f672986069ae046e6148a0e732f10543e60
dream2405 <dream2405@naver.com>
2024-07-19 01:02:04 +0000

    서버 운영 관련 파일 추가 및 수정

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-hbn103&dyq5j&!^960=hqhvxe5vha@3+ku5#@=b7o0dv@nmwy)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '*',
+]
 
 
 # Application definition
