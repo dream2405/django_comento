04c45bb8cec7843966d3be4bc8f78c18e239dac5
dream2405 <dream2405@naver.com>
2024-07-24 14:10:32 +0000

    로그인 페이지 생성

--- comento_main/settings.py
+++ comento_main/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'company',
     'products',
+    'account',
 ]
 
 MIDDLEWARE = [
--- comento_main/urls.py
+++ comento_main/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('company.urls')),
     path('products/', include('products.urls')),
+    path('account/', include('account.urls')),  # TODO: 추후에 프로젝트에 맞게 수정해야 함
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
